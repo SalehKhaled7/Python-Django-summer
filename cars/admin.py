c7908c053b0926315b80bca9f98429e70de6d4d3
salehkhaled7 <lost1994soul@gmail.com>
2020-07-19 18:37:45 +0300

    car model have been added

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from cars.models import Category
+from cars.models import Category, car
 
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['title','model','color','status']
+    list_filter = ['status']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+import datetime
+
+
+def year_choices():
+    return [(r, r) for r in range(1900, datetime.date.today().year + 1)]
+
+
+def current_year():
+    return datetime.date.today().year
 
 
 class Category(models.Model):
     Status = (
-        ('True','Evet'),
-        ('False','Hayir'),
+        ('True', 'Yes'),
+        ('False', 'No'),
     )
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
@@ -12,6 +21,34 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=Status)
     slug = models.SlugField()
-    parent = models.ForeignKey('self' , blank=True , related_name='children' , on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class car(models.Model):
+    Status = (
+        ('True', 'Yes'),
+        ('False', 'NO'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=Status)
+    manufacturer = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    year_of_production = models.IntegerField('year', choices=year_choices(), default=current_year())
+    engine_capacity = models.IntegerField()
+    color = models.CharField(max_length=30)
+    price = models.FloatField()
+    doors = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
